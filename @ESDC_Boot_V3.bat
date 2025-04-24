e12aa21502b7edba10193915ad22a237b9bb21b0
Plangloi <48372629+Plangloi@users.noreply.github.com>
2025-04-24 09:09:32 -0400

    Add files via upload

---
+++ @ESDC_Boot_V3.bat
@@ -0,0 +1,9 @@
+@echo off
+
+timeout /t 70
+
+echo Launching Voicemeeter Banana (64-bit)...
+
+cmd.exe /c start /b "" "C:\Program Files (x86)\VB\Voicemeeter\voicemeeterpro_x64.exe"
+
+:: start "" "C:\Program Files (x86)\VB\Voicemeeter\voicemeeterpro_x64.exe"
